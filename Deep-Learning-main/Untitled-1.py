f29a07597c8495bdf94bc51e9678d84357dad2fe
Eriş Şöylemez <erssylmz12@gmail.com>
2026-01-06 23:27:56 +0300

    Add General Relativity Search Algorithm (GRSA)

--- Deep-Learning-main/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-# Source - https://stackoverflow.com/a
-# Posted by elo80ka, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-12-24, License - CC BY-SA 4.0
-
-import os, sys
-os.path.dirname(sys.executable)
-'C:\\Python25'
